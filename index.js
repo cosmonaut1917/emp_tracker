ca8f27321570599fb4c44668e1dd650ce15844ea
James <jamesdwire66@gmail.com>
2024-03-08 12:43:48 -0700

    added readme

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // const db = require('./db/connection.js');
 // const consoletable = require('console.table');
 const inquirer = require('inquirer');
-
+const db = require('./db/connection');
+const { up } = require('inquirer/lib/utils/readline');
+// list of menu options for user to interact with
 const menu = [
     {
         name: 'menu',
@@ -23,6 +25,7 @@ const menu = [
 
     }
 ];
+// main menu function that prompts the user to select an option
 function mainMenu() {
    inquirer.prompt(menu).then(response => {
    if (response.menu === 'view depts') {
@@ -43,8 +46,8 @@ else if (response.menu === 'add an employee') {
 else if (response.menu === 'add a department') {
     addDepartment();
 }
-else if (response.menu === 'update a department') {
-    updateDepartment();
+else if (response.menu === 'update an employee role') {
+    updateEmployeeRole();
 }
 else if (response.menu === 'delete a role') {
     deleteRole();
@@ -57,12 +60,122 @@ else if (response.menu === 'quit') {
 }})
 }
 mainMenu();
-// function start() {
-//     inquirer
-//         .prompt(menu)
-//         .then(response => {
-//             console.log(response)
-            
-//         })
-// }
-// start();
+function viewDepts() {
+    db.query('SELECT * FROM department', function (err, results) {
+        console.log('viewing departments');
+        console.table(results);
+         mainMenu()
+    });
+    
+   ;
+}
+function viewRoles() {
+    db.query('SELECT * FROM role', function (err, results) {
+        console.log('viewing roles');
+        console.table(results);
+         mainMenu()
+    });
+    
+}
+function viewEmployees() {  
+    db.query('SELECT * FROM employee', function (err, results) {
+        console.log('viewing employees');
+        console.table(results);
+         mainMenu()
+    });
+    
+}
+function addRole() {
+    db.query('SELECT * FROM department', function (err, results) {
+        inquirer.prompt([
+            {
+                name: 'title',
+                type: 'input',
+                message: 'What is the title of the role?'
+            },
+            {
+                name: 'salary',
+                type: 'input',
+                message: 'What is the salary of the role?'
+            },
+            {
+                name: 'department_id',
+                type: 'input',
+                message: 'What is the department id of the role?'
+            }
+        ]).then(response => {
+            db.query('INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)',[response.title, response.salary, response.department_id], function (err, results) {
+                console.log('adding a role');
+                mainMenu();
+            });
+        });
+    });
+}
+// adds an employee
+function addAnEmployee() { 
+inquirer.prompt([
+    {
+        name: 'first_name',
+        type: 'input',
+        message: 'What is the first name of the employee?'
+    },
+    {
+        name: 'last_name',
+        type: 'input',
+        message: 'What is the last name of the employee?'
+    },
+    {
+        name: 'role_id',
+        type: 'input',
+        message: 'What is the role id of the employee?'
+    },
+    {
+        name: 'manager_id',
+        type: 'input',
+        message: 'What is the manager id of the employee?'
+    }
+]).then(response => {
+    db.query('INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)',[response.first_name, response.last_name, response.role_id, response.manager_id], function (err, results) {
+        console.log('adding an employee');
+        mainMenu();
+    });
+});
+}
+// adds a department
+function addDepartment() {
+    inquirer.prompt([
+        {
+            name: 'name',
+            type: 'input',
+            message: 'What is the name of the department?'
+        }
+    ]).then(response => {
+        db.query('INSERT INTO department (department_name) VALUES (?)',[response.name], function (err, results) {
+            console.log('adding a department');
+            mainMenu();
+        });
+    });
+}
+// updates the role of an employee
+function updateEmployeeRole() {
+    inquirer.prompt([
+        {
+            name: 'employee_id',
+            type: 'input',
+            message: 'What is the employee id?'
+        },
+        {
+            name: 'role_id',
+            type: 'input',
+            message: 'What is the role id?'
+        }
+    ]).then(response => {
+        db.query('UPDATE employee SET role_id = ? WHERE id = ?',[response.role_id, response.employee_id], function (err, results) {
+            console.log('updating an employee role');
+            mainMenu();
+        });
+    });
+}
+quit = () => {
+    process.exit();
+}
---
+++ readme.md
@@ -0,0 +1,32 @@
+# Employee Tracker
+
+## Description
+
+This project is an employee tracker system. It allows you to manage your employees' information in a simple and efficient way.
+
+## Installation
+
+To install the project follow the instructions below:
+1. Clone the repository to your local machine.
+2. Navigate to the directory where the repository is located.
+3. Run `npm install` to install all dependencies.
+4. Run `node server.js` to start the application.
+
+## Usage
+
+To use the Employee Tracker, follow the steps below:
+
+1. Run `node server.js` in your terminal.
+2. Follow the prompts to add, view, or update employee information.
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
+## Links
+
+- [GitHub](https://github.com/cosmonaut1917) 
+- [Demo Video](https://youtube.com) 
